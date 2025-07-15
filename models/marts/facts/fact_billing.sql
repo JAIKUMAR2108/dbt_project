67178fa41170e1fe2189a560e67bafee2bf0647f
Jaikumar S <jai2002821@gmail.com>
2025-07-15 11:23:17 +0000

    updated dim_treatment_type table and builded the fact_billing table

--- models/marts/facts/fact_billing.sql
+++ models/marts/facts/fact_billing.sql
@@ -5,9 +5,9 @@
 }}
 
 select
-    concat(billing.sk_bill,patient.sk_patient,treatment.sk_treatment) as appointment_key,
+    treatment.appointment_id as appointment_key,
     patient.sk_patient as patient_key,
-    treatment.sk_treatment as treatment_key,
+    dim_treatment_type.sk_treatment as treatment_key,
     billing.bill_date as bill_date,
     billing.amount as amount,
     billing.payment_method as payment_method,
@@ -17,6 +17,12 @@ from
 on  
     patient.patient_id=billing.patient_id
 join 
-    {{ ref('dim_treatment_type') }} treatment
+    {{ ref('int_treatments') }} treatment
 on      
     treatment.treatment_id=billing.treatment_id
+join
+    {{ ref('dim_treatment_type') }} dim_treatment_type
+on  
+    treatment.treatment_type=dim_treatment_type.treatment_type
+and 
+    treatment.description=dim_treatment_type.description
